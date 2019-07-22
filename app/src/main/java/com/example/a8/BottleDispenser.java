4bf011e188cc805e594c93cb11eaef839f1d13b3
JanettaH <janetta.huoponen@gmail.com>
2019-07-22 21:18:29 +0300

    8.4

--- app/src/main/java/com/example/a8/BottleDispenser.java
+++ app/src/main/java/com/example/a8/BottleDispenser.java
@@ -10,7 +10,12 @@ public class BottleDispenser {
     }
 
     private int pullolkm;
+
+
+
     private double rahaa;
+
+
     private ArrayList<Bottle> pullo_list;
 
     private BottleDispenser() {
@@ -26,4 +31,37 @@ public class BottleDispenser {
         pullo_list.add(new Bottle("Fanta Zero", "Coca-Cola", 1.0, 0.5, 1.95));
 
     }
+    public double getRahaa() {
+        return rahaa;
+    }
+
+    public void lisaaRahaa(int maara) {
+        rahaa += maara;
+    }
+
+    public boolean ostaPullo(int lkm) {
+        Bottle pullo = pullo_list.get(lkm);
+        if(rahaa >= pullo.getHinta()){
+            System.out.println("KACHUNK! " + pullo.getNimi() + " tipahti masiinasta!");
+            rahaa -= pullo.getHinta();
+            pullolkm -= 1;
+            //pullo_list.remove(lkm);
+            return true;
+        } else {
+            System.out.println("Syötä rahaa ensin!");
+            return false;
+        }
+    }
+
+    public void palautaRahat() {
+        System.out.printf("Klink klink. Sinne menivät rahat! Rahaa tuli ulos %.2f€\n", rahaa);
+        rahaa = 0;
+    }
+
+    public ArrayList<Bottle> getPullo_list() {
+        return pullo_list;
+    }
+
+
+
 }
--- app/src/main/java/com/example/a8/MainActivity.java
+++ app/src/main/java/com/example/a8/MainActivity.java
@@ -3,16 +3,82 @@ package com.example.a8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    SeekBar seekbar;
+    TextView text;
+    TextView text2;
+    TextView rahamaara;
+
+    int maara;
+
+    BottleDispenser automaatti = BottleDispenser.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottleDispenser automaatti = BottleDispenser.getInstance();
+        spinner = findViewById(R.id.spinner);
+        seekbar = findViewById(R.id.seekBar);
+        text = findViewById(R.id.textView);
+        text2 = findViewById(R.id.seekBarText);
+        rahamaara = findViewById(R.id.moneyAmount);
 
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                maara = i;
+                text2.setText(Integer.toString(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.pullo_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
     }
+
+    public void lisaaRahaa(View v) {
+        automaatti.lisaaRahaa(maara);
+        rahamaara.setText("Rahaa tällä hetkellä: " + automaatti.getRahaa());
+        text.setText("Klink! Lisää rahaa laitteeseen " + maara + "!");
+    }
+
+    public void ostaPullo(View v) {
+        boolean ostonOnnistuminen = automaatti.ostaPullo(spinner.getSelectedItemPosition());
+        if(!ostonOnnistuminen){
+            text.setText("Syötä rahaa ensin!");
+        } else {
+            text.setText("KACHUNK! Pullo tipahti masiinasta!");
+            rahamaara.setText("Rahaa tällä hetkellä: " + automaatti.getRahaa());
+        }
+    }
+
+    public void palautaRahat(View v) {
+        text.setText("Klink klink. Sinne menivät rahat! Rahaa tuli ulos " + automaatti.getRahaa());
+        automaatti.palautaRahat();
+        rahamaara.setText("Rahaa tällä hetkellä: " + automaatti.getRahaa());
+    }
 }
+
+
